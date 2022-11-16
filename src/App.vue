5cdd2681d442a251f977ccde453aacc9db561c70
JurRodi <rodijurrien@gmail.com>
2022-11-16 12:35:10 +0100

    Feat: add VideoComments

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <script setup>
   import VideoPlayer from './components/VideoPlayer.vue'
   import VideoDetails from './components/VideoDetails.vue'
+  import VideoComments from './components/VideoComments.vue'
 </script>
 
 <template>
   <div class="app">
     <VideoPlayer />
-    <VideoDetails />
+    <div class="app__details">
+      <VideoDetails />
+      <VideoComments />
+    </div>
   </div>
 </template>
 
@@ -14,5 +18,9 @@
   .app {
     display: grid;
     grid-template-columns: 8fr 4fr;
+  }
+  .app__details {
+    display: flex;
+    flex-direction: column;
   }
 </style>
