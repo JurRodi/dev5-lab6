19e83a2983378b606213c1a18bdedc774e9d9f9d
JurRodi <rodijurrien@gmail.com>
2022-11-16 15:27:14 +0100

    Feat: reverse array & add new comment to top

--- src/components/VideoComments.vue
+++ src/components/VideoComments.vue
@@ -9,13 +9,36 @@
             .then(response => response.json())
             .then(data => {
                 comments.comments = data;
+                comments.comments.reverse();
             });
     });
+
+    const addComment = () => {
+        let api_url = "https://lab5-p379.onrender.com/api/v1/messages/";
+        let data = {
+            "id": "",
+            "user": "Anonymous",
+            "text": document.querySelector(".comment").value
+        }
+        fetch(api_url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(data => {
+            comments.comments.unshift(data.data);
+        });
+    }
 </script>
 
 <template>
     <div class="videoComments">
         <h3>Comments</h3>
+        <input type="text" v-model="text" placeholder="Typ something" class="comment">
+        <button @click="addComment">Add comment</button>
         <ul>
             <li v-for="comments in comments.comments" :key="comments.id">
                 <p>{{ comments.user }}</p>
