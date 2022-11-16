df0955b9dea89f501f216e382624dbf7aea77eaf
JurRodi <rodijurrien@gmail.com>
2022-11-16 12:42:11 +0100

    Feat: add title and height styling

--- src/components/VideoComments.vue
+++ src/components/VideoComments.vue
@@ -1,13 +1,16 @@
 <script setup>
-
+    
 </script>
 
 <template>
     <div class="videoComments">
-
+        <h3>Comments</h3>
     </div>
 </template>
 
 <style scoped>
-    
+    div{
+        padding: 0 1rem;
+        height: 80vh;
+    }
 </style>
