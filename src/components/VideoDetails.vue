536ff5f43c900bd82a9234446de91d9f7735d8ca
JurRodi <rodijurrien@gmail.com>
2022-11-16 12:20:21 +0100

    Initial commit

---
+++ src/components/VideoDetails.vue
@@ -0,0 +1,30 @@
+<script setup>
+    import { ref, reactive, onMounted } from 'vue'
+
+    let username = ref('');
+    let description = ref('');
+
+    onMounted(() => {
+        let api_url = "http://localhost:5173/tiktok.json";
+        fetch(api_url)
+            .then(response => response.json())
+            .then(data => {
+                username.value = data.videos[0].username;
+                description.value = data.videos[0].description;
+            });
+    });
+</script>
+
+<template>
+  <div class="videoDetails">
+    <h3>{{ username }}</h3>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<style scoped>
+    div{
+        padding: 0 1rem;
+        height: 30%;
+    }
+</style>
---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,58 @@
+<script setup>
+    import { ref, reactive, onMounted } from 'vue'
+    import 'animate.css'
+
+    let src = ref('');
+    let videos = reactive({ videos: [] });
+    let animation = ref('');
+
+    onMounted(() => {
+        let api_url = "http://localhost:5173/tiktok.json";
+        fetch(api_url)
+            .then(response => response.json())
+            .then(data => {
+                src.value = data.videos[0].video;
+                videos.videos = data.videos;
+            });
+    });
+
+    const nextVideo = () => {
+        animation.value = 'animate__fadeOut';
+        setTimeout(() => {
+            src.value = videos.videos[1].video;
+            animation.value = 'animate__fadeIn';
+        }, 1000);
+    }
+</script>
+
+<template>
+  <div class="video">
+    <div class="controls">
+        <a @click.prevent="nextVideo" href="#" class="controls__next">⬇️</a>
+    </div>
+    <video :src="src" type="video/mp4" controls autoplay muted class="animate__animated" :class="animation"></video>
+  </div>
+</template>
+
+<style scoped>
+    .video{
+        width: 100%;
+    }
+    .controls{
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        z-index: 1;
+    }
+    a{
+        text-decoration: none;
+    }
+    video{
+        width: 100%;
+        max-height: 100vh;
+    }
+</style>
