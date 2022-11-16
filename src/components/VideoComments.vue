f6108e5b88710fb22c1ebfba66e7bf5b061efa4b
JurRodi <rodijurrien@gmail.com>
2022-11-16 12:50:17 +0100

    Feat: add comments from api fetch

--- src/components/VideoComments.vue
+++ src/components/VideoComments.vue
@@ -1,10 +1,27 @@
 <script setup>
-    
+    import { ref, reactive, onMounted } from 'vue'
+
+    let comments = reactive({ comments: [] });
+
+    onMounted(() => {
+        let api_url = "https://lab5-p379.onrender.com/api/v1/messages/";
+        fetch(api_url)
+            .then(response => response.json())
+            .then(data => {
+                comments.comments = data;
+            });
+    });
 </script>
 
 <template>
     <div class="videoComments">
         <h3>Comments</h3>
+        <ul>
+            <li v-for="comments in comments.comments" :key="comments.id">
+                <p>{{ comments.user }}</p>
+                <p>{{ comments.text }}</p>
+            </li>
+        </ul>
     </div>
 </template>
 
